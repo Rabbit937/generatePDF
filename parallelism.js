c6b39f37032a71aaf0c680b5001f7f83adb16ccf
Rabbit937 <hackrabbit@qq.com>
2024-05-22 14:16:51 +0800

    feat: 使用promise.all优化生成pdf逻辑

--- parallelism.js
+++ parallelism.js
@@ -1,13 +1,25 @@
 const { chromium } = require('playwright');
 const fs = require('fs');
+const { PDFDocument } = require('pdf-lib');
 
-(async () => {
+const mergePdfs = async (pdfBuffers) => {
+    const mergedPdf = await PDFDocument.create();
+    for (const pdfBuffer of pdfBuffers) {
+        const pdf = await PDFDocument.load(pdfBuffer);
+        const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
+        copiedPages.forEach((page) => mergedPdf.addPage(page));
+    }
+    return await mergedPdf.save();
+};
+
+const generatePdfFromLinks = async (link, outputPath) => {
     const browser = await chromium.launch({
         executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // 修改为你本地Chrome的路径
         headless: true // 以无头模式运行
     });
+
     const page = await browser.newPage();
-    await page.goto('https://docs.goosefx.io/', { waitUntil: 'domcontentloaded' });
+    await page.goto(link, { waitUntil: 'domcontentloaded' });
 
     // 获取页面中的所有链接
     const links = await page.evaluate(() => {
@@ -17,12 +29,11 @@ const fs = require('fs');
             .filter(href => href.startsWith('https://docs.goosefx.io/'));
     });
 
-
     // 去重
     const uniqueLinks = [...new Set(links)];
-    console.log(uniqueLinks);
 
-    // return false;
+    console.log(uniqueLinks)
+
     const pdfBuffers = [];
 
     // 依次访问每个链接并生成PDF
@@ -32,20 +43,13 @@ const fs = require('fs');
         pdfBuffers.push(pdfBuffer);
     }
 
+    const pdfBuffersPromiseAll = await Promise.all(pdfBuffers);
+    console.log(pdfBuffersPromiseAll)
     await browser.close();
 
-    // 合并所有PDF内容
-    const mergedPdf = await mergePdfs(pdfBuffers);
-    fs.writeFileSync('solend_docs_full.pdf', mergedPdf);
-})();
+    const mergedPdf = await mergePdfs(pdfBuffersPromiseAll);
+    fs.writeFileSync(outputPath, mergedPdf);
+}
 
-const mergePdfs = async (pdfBuffers) => {
-    const PDFDocument = require('pdf-lib').PDFDocument;
-    const mergedPdf = await PDFDocument.create();
-    for (const pdfBuffer of pdfBuffers) {
-        const pdf = await PDFDocument.load(pdfBuffer);
-        const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
-        copiedPages.forEach((page) => mergedPdf.addPage(page));
-    }
-    return await mergedPdf.save();
-};
+
+generatePdfFromLinks('https://docs.goosefx.io/', 'goosefx.pdf')
